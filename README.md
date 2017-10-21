a002a41db142a445b21df8930041c4594f59ff01
Daniel Vaughan <dan@danielvaughan.com>
2017-10-21 09:29:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Deploy
+
+Deploy a Service
+```
+serverless deploy -v
+```
+
+Deploy a Function
+```
+serverless deploy function -f hello
+```
+
+## Run Locally
+
+Run a function locally
+```
+serverless invoke local --function functionName
+```
+
+## Conventions
+Uses [ESLint](https://eslint.org) with standard style guide to check ES6 syntax.
---
+++ api/api.js
@@ -0,0 +1,56 @@
+'use strict'
+const request = require('request')
+
+module.exports.getAllPeople = (event, context, callback) => {
+  const httpUrlForTransaction = process.env.NEO4J_URL
+  const statements = []
+
+  const allPeopleQuery = 'match (p:Person)-[r]->(t:Tag) where p.name is not null and p.hash is not null return p.name, p.hash, p.imageUrl, count(r) order by p.name'
+
+  statements.push({
+    'statement': allPeopleQuery
+  })
+
+  const results = []
+
+  request.post({
+    uri: httpUrlForTransaction,
+    json: {statements: statements}
+  },
+    function (err, resp) {
+      if (err) {
+        const errorResponse = {
+          statusCode: 500,
+          body: err
+        }
+        callback(null, errorResponse)
+      } else {
+        const data = resp.body.results[0].data
+        for (let i = 0; i < data.length; i++) {
+          const person = {}
+          const row = data[i].row
+
+          person.name = row[0]
+          person.hash = row[1]
+          person.tagCount = row[3]
+
+          const imageUrl = row[2]
+
+          if (imageUrl === null) {
+            person.imageUrl = process.env.DEFAULT_PHOTO_URL
+          } else {
+            person.imageUrl = imageUrl
+          }
+          results.push(person)
+        }
+
+        const response = {
+          statusCode: 200,
+          body: results
+        }
+
+        callback(null, response)
+      }
+    }
+  )
+}
