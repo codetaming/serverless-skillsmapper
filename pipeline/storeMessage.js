7489bcf8c103b0680658a2d22fce9b6faf13029d
Daniel Vaughan <dan@danielvaughan.com>
2017-10-22 20:24:49 +0100

    added storeMessages

--- pipeline/storeMessage.js
+++ pipeline/storeMessage.js
@@ -4,13 +4,13 @@ const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'})
 module.exports.storeMessage = (event, context, callback) => {
   const messageText = event.Records[0].Sns.Message
   const message = JSON.parse(messageText)
-  const datetime = new Date().getTime()
+  const datetime = new Date().getTime().toString()
   console.log(datetime)
   dynamodb.putItem({
     'TableName': process.env.MESSAGES_RECEIVED_TABLE,
     'Item': {
       'id': {'S': message.id},
-      'time': {'N': datetime},
+      'timestamp': {'S': datetime},
       'source': {'S': message.source},
       'type': {'S': message.type},
       'tags': {'S': message.tags}
