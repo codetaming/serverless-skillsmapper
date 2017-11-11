802fbab5500582a2b3fbf2c4ffaa09573dc8f323
Daniel Vaughan <dan@danielvaughan.com>
2017-11-11 19:04:13 +0000

    added ses permission

--- aws-scripts/connect-to-ec2.sh
+++ aws-scripts/connect-to-ec2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh -i serverless.pem ec2-user@ec2-34-249-121-93.eu-west-1.compute.amazonaws.com
+ssh -i serverless.pem ec2-user@ec2-52-214-238-194.eu-west-1.compute.amazonaws.com
