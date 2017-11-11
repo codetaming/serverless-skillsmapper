91d995d9f76c40ee061b1663b4df28e48cae6289
Daniel Vaughan <dan@danielvaughan.com>
2017-11-11 15:57:27 +0000

    added tag set valid

---
+++ pipeline/storeTagValidity.js
@@ -0,0 +1,40 @@
+const request = require('request')
+
+module.exports.storeTagValidity = (event, context, callback) => {
+  const httpUrlForTransaction = process.env.NEO4J_URL
+  const message = event
+  const tagsSetValid = []
+
+  const updateTagQuery = 'MATCH (tag:Tag { name: {name} })' +
+    'SET tag.valid = true ' +
+    'SET tag.validated = timestamp() '
+
+  let output = {}
+
+  for (const tag of message.validTags) {
+    const statements = []
+    statements.push({
+      'statement': updateTagQuery,
+      'parameters': {name: tag, valid: message.valid}
+    })
+    request.post({
+      uri: httpUrlForTransaction,
+      json: {statements: statements}
+    },
+      function (err, resp) {
+        if (err) {
+          console.log(err)
+        } else {
+          if (resp.body.errors.length > 0) {
+            console.log(JSON.stringify(resp.body.errors))
+          } else {
+            tagsSetValid.push(tag)
+            if (tagsSetValid.length === message.validTags.length) {
+              output.tagsSetValid = tagsSetValid
+              callback(null, output)
+            }
+          }
+        }
+      })
+  }
+}
