191f15958f6f129c7d25d97bb57c068fa0172120
Daniel Vaughan <dan@danielvaughan.com>
2017-11-14 08:40:01 +0000

    added angular rendering of people

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-
-import { AppComponent } from './app.component';
+import {PeopleComponent} from "./people.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    PeopleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [PeopleComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ client/src/app/people.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import * as _ from 'lodash';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+interface Person {
+  name: string;
+  hash: string;
+  imageUrl: string;
+  tagCount: number;
+}
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./people.component.css'],
+  template: `
+    <ul *ngIf="people$ | async as people else noData">
+      <div class="person" *ngFor="let person of people">
+        <div>
+          <img class="person-photo" src="{{person.imageUrl}}" title="{{person.name}}">
+        </div>
+        <div class="person-name">
+          {{person.name}} [{{person.tagCount}}]
+        </div>
+      </div>
+    </ul>
+    <ng-template #noData>No Data Available</ng-template>
+  `
+})
+
+export class PeopleComponent implements OnInit {
+
+  people$: Observable<Person[]>;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.people$ = this.http
+      .get<Person[]>('https://api.skillsmapper.org/profiles')
+      .map(data => _.values(data))
+      .do(console.log);
+  }
+}
