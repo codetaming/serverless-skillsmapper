c7bfb9e8c4df431e9657a3226fc4e0f4b3c81a37
Daniel Vaughan <dan@danielvaughan.com>
2018-02-25 18:55:57 +0000

    added sqs queue putting for repopulate

--- pipeline/processBatchedMessage.js
+++ pipeline/processBatchedMessage.js
@@ -5,6 +5,7 @@ const stepfunctions = new AWS.StepFunctions()
 const sqs = new AWS.SQS()
 
 module.exports.processBatchedMessage = (event, context, callback) => {
+  console.log('processBatchedMessage')
   const queueUrl = process.env.BATCH_MESSAGE_QUEUE_URL
 
   var params = {
@@ -15,6 +16,9 @@ module.exports.processBatchedMessage = (event, context, callback) => {
     if (err) {
       console.log('Receive Error', err)
     } else if (data.Messages) {
+      const message = JSON.parse(data.Messages[0].Body)
+      console.log('Processing: ' + JSON.stringify(message))
+      startStateMachine(message)
       var deleteParams = {
         QueueUrl: queueUrl,
         ReceiptHandle: data.Messages[0].ReceiptHandle
@@ -26,6 +30,23 @@ module.exports.processBatchedMessage = (event, context, callback) => {
           console.log('Message Deleted', data)
         }
       })
+    } else {
+      console.log('No message')
     }
+    callback(null, 'finished')
+  })
+}
+
+function startStateMachine (payload) {
+  const stateMachineArn = process.env.STATEMACHINE_ARN
+  console.log('Starting state machine: ' + stateMachineArn)
+  const params = {
+    stateMachineArn,
+    'input': JSON.stringify({message: payload})
+  }
+  console.log(JSON.stringify(params))
+  stepfunctions.startExecution(params, function (err, data) {
+    if (err) console.log(err, err.stack)
+    else console.log(data)
   })
 }
